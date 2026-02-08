df116691c0452101804192073b87ad4b6c2483c9
AntonioBarcel0 <antoniogibarber99@gmail.com>
2026-02-08 18:25:29 +0100

    log-btn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,18 +14,28 @@ function Header({ onMenuClick }) {
       <div className="logo">
         <img 
           src="https://res.cloudinary.com/dlmnchkjg/image/upload/v1770289985/Captura_de_pantalla_2026-02-05_a_las_12.12.57_fhymgg.png" 
-          alt="Carrito"
+          alt="logo"
           className="logo-image"
         />
       </div>
 
-      <button className="cart-btn" aria-label="Carrito de compra">
-        <img 
-          src="https://res.cloudinary.com/dlmnchkjg/image/upload/v1770290214/Captura_de_pantalla_2026-02-05_a_las_12.16.48_syfomx.png" 
-          alt="Menú hamburguesa"
-          className="cart-icon"
-        />
-      </button>
+      <div className="right-section">
+        <button className="log-btn" aria-label="registro">
+          <img 
+            src="https://res.cloudinary.com/dlmnchkjg/image/upload/v1770290764/Captura_de_pantalla_2026-02-05_a_las_12.25.59_k694e7.png" 
+            alt="registro"
+            className="log-icon"
+          />
+        </button>
+
+        <button className="cart-btn" aria-label="Carrito de compra">
+          <img 
+            src="https://res.cloudinary.com/dlmnchkjg/image/upload/v1770290214/Captura_de_pantalla_2026-02-05_a_las_12.16.48_syfomx.png" 
+            alt="Carrito compra"
+            className="cart-icon"
+          />
+        </button>
+      </div>
     </header>
   );
 }
